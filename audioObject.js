2a98596ac8ec6b922b21926db071ab09ecea6805
mohammadhossein <m.h.dolatabadi.a@gmail.com>
2022-04-19 12:25:47 +0430

    Initialize project

---
+++ audioObject.js
@@ -0,0 +1,56 @@
+[
+    "rs",
+    "webrtc::2",
+    {
+        "consumerId": "b691c918-e369-49f1-8d52-183de85ff0ea",
+        "kind": "audio",
+        "rtpParameters": {
+            "codecs": [
+                {
+                    "mimeType": "audio/opus",
+                    "payloadType": 111,
+                    "clockRate": 48000,
+                    "channels": 2,
+                    "parameters": {
+                        "minptime": 10,
+                        "useinbandfec": 1
+                    },
+                    "rtcpFeedback": []
+                }
+            ],
+            "headerExtensions": [
+                {
+                    "uri": "urn:ietf:params:rtp-hdrext:sdes:mid",
+                    "id": 1,
+                    "encrypt": false,
+                    "parameters": {}
+                },
+                {
+                    "uri": "http://www.webrtc.org/experiments/rtp-hdrext/abs-send-time",
+                    "id": 4,
+                    "encrypt": false,
+                    "parameters": {}
+                },
+                {
+                    "uri": "urn:ietf:params:rtp-hdrext:ssrc-audio-level",
+                    "id": 10,
+                    "encrypt": false,
+                    "parameters": {}
+                }
+            ],
+            "encodings": [
+                {
+                    "ssrc": 452299228
+                }
+            ],
+            "rtcp": {
+                "cname": "GtpjzdA/iQNM3QC8",
+                "reducedSize": true,
+                "mux": true
+            },
+            "mid": "0"
+        },
+        "type": "simple",
+        "producerPaused": false
+    }
+]
